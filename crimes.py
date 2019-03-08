d2432a2911c8ce112c80b1fb7712b01f0ccbb63f
Harley Glover-Newson <harleygn@gmail.com>
2019-03-08 14:43:01 +0000

    Load csv files and join to one dataset

---
+++ crimes.py
@@ -0,0 +1,19 @@
+# import geodist
+import pandas as pd
+import os
+
+
+def load_crime_data(source_directory):
+    crime_files = os.listdir(source_directory)
+    all_crimes = []
+    for filename in crime_files:
+        crime_dataframe = pd.read_csv(source_directory + filename)
+        all_crimes.append(crime_dataframe)
+    return pd.concat(all_crimes)
+
+
+def get_lat_lon(dataframe):
+    return dataframe[['Longitude', 'Latitude']]
+
+
+print(get_lat_lon(load_crime_data('Devon_and_Cornwall_crime_data_2018/')))
