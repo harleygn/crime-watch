61680099c818f1531d0144449d6e23bb7304d8fc
kalonmartin <kalonlajd@gmail.com>
2019-03-18 08:26:47 +0000

    tkinter delete

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import userinput as ui
 
 postcode, radius = ui.setup()
 
-
+print("Searching for crimes within " + str(radius) + "km of " + postcode)
 
--- userinput.py
+++ userinput.py
@@ -1,41 +1,6 @@
 import re
 from tkinter import *
 
-
-master = Tk()
-master.geometry("250x500")
-e = Entry(master)
-e.place(x=65, y=225)
-e.focus_set()
-
-def callback():
-    print (e.get())
-
-b = Button(master, text="Start", width=10, command=callback)
-b.place(x=85, y=375)
-
-variable = StringVar(master)
-variable.set("Radius") # default value
-
-w = OptionMenu(master, variable, "1", "2", "5")
-w.place(x=85, y=275)
-
-mainloop()
-
-e = Entry(master, width=50)
-
-postcode_text = e.get()
-radius_text = e.get()
-
-content = StringVar()
-entry = Entry(parent, text=caption, textvariable=content)
-
-text = content.get()
-content.set(text)
-
-root = Tk()
-root.mainloop()
-
 def setup():
     valid = False
 
@@ -55,7 +20,7 @@ def setup():
         if int(radius) <= 5:
             valid = True
         else:
-            print("Please enter a radius between 0 and 5.")
+            print("Please enter a NUMBER between 1 and 5.")
 
     return postcode, radius
 
