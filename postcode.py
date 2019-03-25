9a27c2410dc259639a4f7616c22bb72b4bf9bb20
Harley Glover-Newson <harleygn@gmail.com>
2019-03-25 15:53:58 +0000

    postcode.csv filepath is system independent

--- postcode.py
+++ postcode.py
@@ -5,7 +5,7 @@ Created on Tue Mar 19 08:30:11 2019
 @author: Kalon Martin , Matthew Penswick
 """
 
-file_path = "C:/Users/Kalon Martin/Documents/GitHub/crime-watch/postcodes.csv"
+file_path = "./postcodes.csv"
 
 def load_postcodes():
     '''
