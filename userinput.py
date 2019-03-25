d4ea379529e04e72883b06a97d1bc1926859d910
Harley Glover-Newson <harleygn@gmail.com>
2019-03-25 15:49:42 +0000

    Cleaned up code

--- userinput.py
+++ userinput.py
@@ -1,23 +1,23 @@
 from postcode import validate_postcode
-from tkinter import *
+
 
 def setup():
     valid = False
-    radius_inputs = [1,2,50]
+    radius_inputs = [1, 2, 50]
 
-    while valid == False:
+    while not valid:
         print("Enter a postcode...")
         postcode = input()
         postcode = postcode.upper()
-        
+
         if validate_postcode(postcode)[0]:
             valid = True
         else:
             print("Invalid postcode. Please enter in a valid format e.g. AB1 2CD or EG11 2EG")
-            
+
     valid = False
 
-    while valid == False:
+    while not valid:
         print("Enter a radius...")
         radius = input()
         if int(radius) in radius_inputs:
@@ -26,4 +26,3 @@ def setup():
             print("Please enter a valid radius (1 , 2 , 5)")
 
     return postcode, radius
-
